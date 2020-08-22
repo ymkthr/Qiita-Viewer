c0bf01fa3ed485358a638e359940ad11cbc926bb
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-08-22 14:09:24 +0900

    Initial Commit

---
+++ Qiita ViewerTests/Qiita_ViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Qiita_ViewerTests.swift
+//  Qiita ViewerTests
+//
+//  Created by Katsuhiro Yamauchi on 2020/08/22.
+//  Copyright © 2020 Katsuhiro Yamauchi. All rights reserved.
+//
+
+import XCTest
+@testable import Qiita_Viewer
+
+class Qiita_ViewerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
