f989ae1a66af0bd5ed30169b7fb89f4bb39694a2
Katsuhiro Yamauchi <katsu.ymw@gmail.com>
2020-08-25 00:22:56 +0900

    feature: 投稿の情報を表示
    add: ユーザーアイコンを表示する
    
    move: Extensionディレクトリに移動
    
    remove: 不要なimportを削除

---
+++ Qiita Viewer/Extension/Article+ComputedProperty.swift
@@ -0,0 +1,23 @@
+//
+//  extensionArticle.swift
+//  Qiita Viewer
+//
+//  Created by Katsuhiro Yamauchi on 2020/08/25.
+//  Copyright © 2020 Katsuhiro Yamauchi. All rights reserved.
+//
+
+extension Article {
+    var date: String  {
+           get {
+               let date = self.created_at.prefix(10)
+               return String(date)
+           }
+       }
+    var lgtm: String {
+        get {
+            let int = likes_count.description
+            return int
+        }
+    }
+    
+}
